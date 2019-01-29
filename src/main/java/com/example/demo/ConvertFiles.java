ab5eec51024607289e0ac69780b662d83b10ec76
cassandraGoose <cassandra.torske@gmail.com>
2019-01-29 15:50:47 -0700

    update for static methods

--- src/main/java/com/example/demo/ConvertFiles.java
+++ src/main/java/com/example/demo/ConvertFiles.java
@@ -14,9 +14,8 @@ import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.io.File;
 import java.nio.file.Paths;
-
+import java.util.UUID;
 
 @RestController
 public class ConvertFiles {
@@ -24,13 +23,13 @@ public class ConvertFiles {
   // Hit localhost:8080/convert with a body of form-data, key: 'file' value: uploaded file
   @RequestMapping(value = "/convert", method = RequestMethod.POST)
   public ResponseEntity<?> convertFile(@RequestParam("file") MultipartFile file) throws IOException, Exception {
-    Converter converter = new Converter();
     byte[] bytes = file.getBytes();
     // choose whatever input/output formats you'd like below
-    byte[] convertedBytes = converter.convertFileFromByteArray(bytes, InputFormat.PDF, OutputFormat.TIF);
+    byte[] convertedBytes = Converter.convertFileFromByteArray(bytes, InputFormat.PDF, OutputFormat.TIF);
     // choose wherever you'd like below
-    Files.write(Paths.get("/Users/cassandratorske/Workspace/saved.tif"), convertedBytes);
-    return new ResponseEntity<String>("Please check that this worked.", HttpStatus.OK);
+    String uuid = UUID.randomUUID().toString();
+    Files.write(Paths.get("./" + uuid + ".tif"), convertedBytes);
+    return new ResponseEntity<String>("Provide your desired response here.", HttpStatus.OK);
   }
 
 
@@ -40,12 +39,12 @@ public class ConvertFiles {
   // key: "outputFormat", value: "TIF" (or whatever format you are usihng)
   @RequestMapping(value = "/convert-with-params", method = RequestMethod.POST)
   public ResponseEntity<?> convertFile(@RequestParam("inputFormat") InputFormat input, @RequestParam("outputFormat") OutputFormat output, @RequestParam("file") MultipartFile file) throws IOException, Exception {
-    Converter converter = new Converter();
     byte[] bytes = file.getBytes();
-    byte[] convertedBytes = converter.convertFileFromByteArray(bytes, input, output);
+    byte[] convertedBytes = Converter.convertFileFromByteArray(bytes, input, output);
     // choose where to save and name your file below
-    Files.write(Paths.get("./saved.tif"), convertedBytes);
-    return new ResponseEntity<String>("Please check that this worked", HttpStatus.OK);
+    String uuid = UUID.randomUUID().toString();
+    Files.write(Paths.get("./" + uuid + ".tif"), convertedBytes);
+    return new ResponseEntity<String>("Provide your desired response here.", HttpStatus.OK);
   }
 
   // Hit localhost:8080/convert-from-path with a body for form-data
@@ -54,9 +53,10 @@ public class ConvertFiles {
   // key: "outputFormat", value: "TIF" (or whatever format you are usihng)
   @RequestMapping(value = "/convert-from-path", method = RequestMethod.POST) 
   public ResponseEntity<?> convertFile(@RequestParam("inputFormat") InputFormat input, @RequestParam("outputFormat") OutputFormat output, @RequestParam("path") String path) throws IOException, Exception {
-    Converter converter = new Converter();
-    byte[] convertedBytes = converter.convertFileFromPath(path, input, output);
-    Files.write(Paths.get("./saved.tif"), convertedBytes);
-    return new ResponseEntity<String>("Please check that this worked", HttpStatus.OK);
+    byte[] convertedBytes = Converter.convertFileFromPath(path, input, output);
+    // choose where to save and name your file below
+    String uuid = UUID.randomUUID().toString();
+    Files.write(Paths.get("./" + uuid + ".tif"), convertedBytes);
+    return new ResponseEntity<String>("Provide your desired response here.", HttpStatus.OK);
   }
 }
